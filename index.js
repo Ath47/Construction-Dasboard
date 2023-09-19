31886770a140e0d1c24d46b8a54ab7985f241d68
Atharva K <atharva47kulkarni@gmail.com>
2023-09-19 21:34:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+// Sample logout functionality (replace with actual authentication)
+document.getElementById('logout').addEventListener('click', function () {
+    window.location.href = 'http://127.0.0.1:5501/login.html';
+});
+
+
+
+// Sample data loading (replace with actual data retrieval)
+window.addEventListener('DOMContentLoaded', function () {
+    loadSafetyData();
+    loadEfficiencyData();
+    loadMachineData();
+});
+
+function loadSafetyData() {
+    // Replace with code to load safety data and populate the "safety-chart" div
+    // Example: Fetch data from an API and use a charting library to create visualizations
+}
+
+function loadEfficiencyData() {
+    // Replace with code to load efficiency data and populate the "efficiency-chart" div
+    // Example: Fetch data from an API and use a charting library to create visualizations
+}
+
+function loadMachineData() {
+    // Replace with code to load machine status data and populate the "machine-chart" div
+    // Example: Fetch data from an API and use a charting library to create visualizations
+}
